5837e8818bec36464bd8f211bfaebb28b524d8eb
gulls12 <gulls12@gulls12.com>
2021-11-14 16:18:07 +0300

    First version - all logic was created.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,59 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    protected int size = 0;
 
     void clear() {
+        //storage = new Resume[10000];
+        for (int x = 0; x <= size(); x++){
+            storage[x] = null;
+        }
     }
 
     void save(Resume r) {
+        storage[size]=r;
+        size++;
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume r = new Resume();
+        for(int x = 0; x <= size(); x++){
+            if (storage[x].toString() == uuid) {
+                r = storage[x];
+            }
+            break;
+        }
+        //return (Resume) Arrays.stream(storage).filter(s -> Objects.nonNull(s)).filter(s -> s.toString() == uuid);
+        return r;
+
     }
 
     void delete(String uuid) {
+        for (int x = 0; x<=size(); x++) {
+            if(storage[x].toString() == uuid){
+                for(int y = x; y <=size()-1; y++){
+                    storage[y] = storage[y+1];
+                }
+                break;
+            }
+        }
+        //storage = Arrays.stream(storage).filter(s -> Objects.nonNull(s)).filter(s -> s.toString() != uuid).toArray(Resume[]::new);
+        size--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] newStorage = new Resume[size()];
+        for (int x = 0; x <= size(); x++){
+            newStorage[x] = storage[x];
+        }
+        //newStorage = Arrays.stream(storage).filter(s -> Objects.nonNull(s)).toArray(Resume[]::new);
+        return newStorage;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
